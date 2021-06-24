f58900d507f18df3d1a57163e065d8bc542ade78
Krati <krati27mishra@gmail.com>
2021-06-24 19:06:00 +0530

    Integrated payment gateway

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("tracker/",views.tracker,name="TrackingStatus"),
     path("search/",views.search,name="Search"),
     path("products/<int:myid>",views.productview,name="ProductView"),
-    path("checkout/",views.checkout,name="Checkout")
+    path("checkout/",views.checkout,name="Checkout"),
+    path("handlerequest/",views.handlerequest,name="HandleRequest"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from . models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
-
+from django.views.decorators.csrf import csrf_exempt
+from PayTm import Checksum
+MERCHANT_KEY = 'kbzk1DSbJiV_O3p5';
 
 def index(request):
     allProds = []
@@ -72,21 +74,49 @@ def checkout(request):
     if request.method == "POST":
         items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
+        amount = request.POST.get('amount', '')
         email = request.POST.get('email', '')
-        address = request.POST.get('address1', '') + \
-            " " + request.POST.get('address2', '')
+        address = request.POST.get('address1', '') +" " + request.POST.get('address2', '')
         city = request.POST.get('city', '')
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
         order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
-                       state=state, zip_code=zip_code, phone=phone)
+                       state=state, zip_code=zip_code, phone=phone,amount=amount)
         order.save()
         update = OrderUpdate(order_id=order.order_id,
                              update_desc="The order has been placed")
         update.save()
         thank = True
         id = order.order_id
-        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+        # return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+        param_dict = {
 
+                    'MID': 'WorldP64425807474247',
+                    'ORDER_ID': str(order.order_id),
+                    'TXN_AMOUNT': str(amount),
+                    'CUST_ID': email,
+                    'INDUSTRY_TYPE_ID': 'Retail',
+                    'WEBSITE': 'WEBSTAGING',
+                    'CHANNEL_ID': 'WEB',
+                    'CALLBACK_URL':'http://127.0.0.1:8000/shop/handlerequest/',
+            }
+        param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict,MERCHANT_KEY)
+        return render(request,'shop/paytm.html',{'param_dict':param_dict})
     return render(request, 'shop/checkout.html')
+
+@csrf_exempt
+def handlerequest(request):
+    form = request.POST
+    response_dict= {}
+    for i in form.keys():
+        response_dict[i]=form[i]
+        if i =='CHECKSUMHASH':
+            checksum=form[i]
+    verify = Checksum.verify_checksum(response_dict,MERCHANT_KEY,checksum)
+    if verify:
+        if response_dict['RESPCODE']=='01':
+            print('order successful')
+        else:
+            print('Order was not successful because'+response_dict['RESPMSG'])
+    return render(request,'shop/paymentstatus.html',{'response':response_dict})
